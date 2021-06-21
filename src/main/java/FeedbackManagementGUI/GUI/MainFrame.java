d82f91d0f5d04e14c584cec4993317ebafe4318a
forumber <kidobeko@gmail.com>
2021-06-21 22:24:08 +0300

    Remove unused imports

--- src/main/java/FeedbackManagementGUI/GUI/MainFrame.java
+++ src/main/java/FeedbackManagementGUI/GUI/MainFrame.java
@@ -5,14 +5,6 @@
  */
 package FeedbackManagementGUI.GUI;
 
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-
 /**
  *
  * @author Berkay
