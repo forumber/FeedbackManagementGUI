3bf51a626d493fb335a9abc3de4156a0f025eced
forumber <kidobeko@gmail.com>
2021-06-21 22:31:07 +0300

    Remove comments

--- src/main/java/FeedbackManagementGUI/MainApplication.java
+++ src/main/java/FeedbackManagementGUI/MainApplication.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package FeedbackManagementGUI;
 
 import FeedbackManagementGUI.GUI.MainFrame;
 
-/**
- *
- * @author Berkay
- */
 public class MainApplication {
     
     public MainFrame mainFrame;
@@ -21,6 +12,7 @@ public class MainApplication {
     }
 
     public static void main(String[] args) {
+        
         new MainApplication().mainFrame.start();
     }
     
