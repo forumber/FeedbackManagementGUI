f024253f1fec5d32bec63541dd8fed1cac3f6338
forumber <kidobeko@gmail.com>
2021-06-26 12:33:08 +0300

    Remove unused action

--- src/main/java/FeedbackManagement/GUI/CustomerRegisterPanel.java
+++ src/main/java/FeedbackManagement/GUI/CustomerRegisterPanel.java
@@ -31,12 +31,6 @@ public class CustomerRegisterPanel extends javax.swing.JPanel {
 
         jLabel1.setText("Phone Number:");
 
-        CustPhoneTF.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                CustPhoneTFActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -60,10 +54,6 @@ public class CustomerRegisterPanel extends javax.swing.JPanel {
 
         jLabel1.getAccessibleContext().setAccessibleName("CustPhoneNumber");
     }// </editor-fold>//GEN-END:initComponents
-
-    private void CustPhoneTFActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CustPhoneTFActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_CustPhoneTFActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
